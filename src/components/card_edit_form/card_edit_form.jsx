414c2e91006e1ffb9ca8623135c5c4c606b03a30
kunwoo5012@naver.com <aepeul@aepeul-ui-MacBookPro.local>
2021-02-02 15:11:58 +0900

    handle state change on editor

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import Button from "../button/button";
 import ImageFileInput from "../image_file_input/image_file_input";
 import styles from "./card_edit_form.module.css";
 
-const CardEditForm = ({ card }) => {
+const CardEditForm = ({ card, updateCard, deleteCard }) => {
+    const nameRef = useRef();
+    const companyRef = useRef();
+    const themeRef = useRef();
+    const titleRef = useRef();
+    const emailRef = useRef();
+    const messageRef = useRef();
+
     const {
         name,
         company,
@@ -14,7 +21,20 @@ const CardEditForm = ({ card }) => {
         fileName,
         fileURL,
     } = card;
-    const onSubmit = () => {};
+
+    const onChange = (event) => {
+        if (event.currentTarget == null) {
+            return;
+        }
+        event.preventDefault();
+        updateCard({
+            ...card,
+            [event.currentTarget.name]: event.currentTarget.value,
+        });
+    };
+    const onSubmit = (event) => {
+        deleteCard(card);
+    };
     return (
         <form className={styles.form}>
             <input
@@ -22,34 +42,50 @@ const CardEditForm = ({ card }) => {
                 type="text"
                 name="name"
                 value={name}
+                ref={nameRef}
+                onChange={onChange}
             />
             <input
                 className={styles.input}
                 type="text"
                 name="company"
                 value={company}
+                ref={companyRef}
+                onChange={onChange}
             />
-            <select className={styles.select} name="theme" value={theme}>
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-                <option value="colorfult">Colorful</option>
+            <select
+                className={styles.select}
+                name="theme"
+                value={theme}
+                ref={themeRef}
+                onChange={onChange}
+            >
+                <option value="light">light</option>
+                <option value="dark">dark</option>
+                <option value="colorful">colorful</option>
             </select>
             <input
                 className={styles.input}
                 type="text"
                 name="title"
                 value={title}
+                ref={titleRef}
+                onChange={onChange}
             />
             <input
                 className={styles.input}
                 type="text"
                 name="email"
                 value={email}
+                ref={emailRef}
+                onChange={onChange}
             />
             <textarea
                 className={styles.textarea}
                 name="message"
                 value={message}
+                ref={messageRef}
+                onChange={onChange}
             />
             <div className={styles.fileInput}>
                 <ImageFileInput />
